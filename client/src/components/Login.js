450516213f7525616916f8e64ccbe217ea199c9c
Tomás Almeida <tlc.almeida@campus.fct.unl.pt>
2022-06-03 02:15:11 +0100

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,33 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Button, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { userLogin } from '../services/user';
 
-export default function Login() {
+export default function Login(props) {
     const [user, setUser] = useState({ username: '', password: '' });
+    const [error, setError] = useState(null);
 
     const handleUsernameChange = (e) => {
-        setTasks({ ...user, username: e.target.value });
+        setUser({ ...user, username: e.target.value });
     }
     const handlePasswordChange = (e) => {
-        setTasks({ ...user, password: e.target.value });
+        setUser({ ...user, password: e.target.value });
     }
 
+    async function userLoginClick() {
+        try {
+            if(user.username !== '' && user.password !== ''){                
+                console.log(user);
+                const result = await userLogin(user);
+                console.log("userLoginClick result",result.data);
+                props.onUserLogin(result.data);
+            }
+            else{
+                setError('Fill the required fields!');
+            }            
+        } catch (error) {
+          console.log(error);
+        }
+    }
 
     return (
         <form>            
@@ -18,7 +35,7 @@ export default function Login() {
                 variant="outlined"
                 size="small"
                 sx={{ width: "80%" }}
-                value={tasks.currentTask}
+                value={user.username}
                 required={true}
                 onChange={handleUsernameChange}
                 placeholder="Username"
@@ -27,7 +44,7 @@ export default function Login() {
                 variant="outlined"
                 size="small"
                 sx={{ width: "80%" }}
-                value={tasks.currentTask}
+                value={user.password}
                 required={true}
                 onChange={handlePasswordChange}
                 type="password"
@@ -37,10 +54,19 @@ export default function Login() {
             sx={{ height: "40px", width: '200px' }}
             color="primary"
             variant="outlined"
-            type="submit"
-          >
-            Login
-          </Button>
+            //type="submit"
+            onClick={userLoginClick}
+            >
+                Login
+            </Button>
+            <Button
+            sx={{ height: "40px", width: '200px' }}
+            color="primary"
+            variant="outlined"
+//            onClick={userLogin}
+            >
+                Sign Up
+            </Button>
         </form>
     )
 }
